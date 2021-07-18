6ab0d60c71c7bc2808d7c21c0fc15b45dcbd47aa
Naveneetha Vasudevan <naveneetha@yahoo.com>
2021-07-18 12:30:38 +0100

    use struct for cfg mutations

--- src/cfg/mutate.rs
+++ src/cfg/mutate.rs
@@ -4,7 +4,10 @@ use rand;
 use rand::prelude::SliceRandom;
 use rand::thread_rng;
 
-use crate::cfg::{Cfg, LexSymbol, RuleAlt, TermSymbol};
+use crate::cfg::{Cfg, LexSymbol, TermSymbol};
+use std::io::Write;
+
+const MAX_ITER_CNT: usize = 500;
 
 #[derive(Debug)]
 pub(crate) struct CfgMutateError {
@@ -19,87 +22,139 @@ impl CfgMutateError {
     }
 }
 
-fn terminal_alts_indices(cfg: &Cfg) -> HashMap<String, Vec<usize>> {
-    let mut term_alts: HashMap<String, Vec<usize>> = HashMap::new();
-    for rule in &cfg.rules {
-        let mut term_indices: Vec<usize> = vec![];
-        for (i, alt) in rule.rhs.iter().enumerate() {
-            let mut has_terms = false;
-            for sym in &alt.lex_symbols {
-                if let LexSymbol::Term(_) = sym {
-                    has_terms = true;
+pub(crate) struct CfgMutation<'a> {
+    cfg: &'a Cfg,
+    terminal_indices: HashMap<String, Vec<Vec<usize>>>,
+    non_terms: Vec<String>,
+    terms: Vec<&'a TermSymbol>
+}
+
+impl<'a> CfgMutation<'a> {
+    fn new(cfg: &'a Cfg) -> Self {
+        Self {
+            cfg,
+            terminal_indices: Default::default(),
+            non_terms: vec![],
+            terms: vec![]
+        }
+    }
+
+    /// Calculates the indices of alternatives for each rule
+    fn terminal_indices(&mut self) {
+        let mut term_indices_map: HashMap<String, Vec<Vec<usize>>> = HashMap::new();
+        for rule in &self.cfg.rules {
+            let mut rule_i: Vec<Vec<usize>> = vec![];
+            for (i, alt) in rule.rhs.iter().enumerate() {
+                let mut alt_i: Vec<usize> = vec![];
+                for (j, sym) in alt.lex_symbols.iter().enumerate() {
+                    if let LexSymbol::Term(_) = sym {
+                        alt_i.push(j);
+                    }
+                }
+                rule_i.push(alt_i);
+            }
+            term_indices_map.insert(rule.lhs.to_owned(), rule_i);
+        }
+
+        self.terminal_indices = term_indices_map;
+    }
+
+    /// Returns a list of non-terminals which have alternatives with terminals in them
+    fn nt_alt_with_terminals(&mut self) {
+        let mut keys: Vec<String> = vec![];
+        for (k, values) in &self.terminal_indices {
+            for v in values {
+                if v.len() > 0 {
+                    keys.push(k.to_owned());
                     break;
                 }
             }
-            if has_terms {
-                term_indices.push(i);
+        }
+        self.non_terms = keys;
+    }
+
+    fn instantiate(&mut self) {
+        self.terminal_indices();
+        self.nt_alt_with_terminals();
+        self.terms = self.cfg.terminals();
+    }
+
+    /// Returns the number of possible mutations where terminals occur
+    fn mut_cnt(&self) -> usize {
+        self.terminal_indices.values()
+            .map(|u|
+                u.iter().map(|v| v.len()).sum::<usize>()
+            )
+            .sum::<usize>()
+    }
+
+    /// Returns a tuple of indices of terminal location
+    fn alt_with_terminals(&self, nt: &str) -> (usize, usize) {
+        let alt_with_terms = self.terminal_indices.get(nt).unwrap();
+        let mut alt_indices: Vec<usize> = vec![];
+        for (i, v) in alt_with_terms.iter().enumerate() {
+            if v.len() > 0 {
+                alt_indices.push(i);
             }
         }
-        if !term_indices.is_empty() {
-            term_alts.insert(rule.lhs.to_owned(), term_indices);
-        }
+        let j = alt_indices.choose(&mut thread_rng()).unwrap();
+        let terminal_indices = &alt_with_terms[*j];
+        let terminal_i = terminal_indices.choose(&mut thread_rng()).unwrap();
+        // println!("{}: alt_with_terms: {:?}, j: {}, term_i: {}", nt, alt_with_terms, j, terminal_i);
+
+        (*j, *terminal_i)
     }
 
-    term_alts
-}
+    /// Mutates the give alternative using the base set of terminals `terms`
+    fn mutate(&mut self) -> Result<Cfg, CfgMutateError> {
+        let nt = self.non_terms.choose(&mut thread_rng()).unwrap();
+        let (alt_i, term_j) = self.alt_with_terminals(&nt);
+        let mut cfg = self.cfg.clone();
+        let mut alt = cfg.get_alt_mut(nt, alt_i)
+            .ok_or_else(||
+                CfgMutateError::new(
+                    format!("Failed to get alternative for non-terminal {} (index: {})",
+                            nt,
+                            alt_i
+                    )
+                ))?;
+        let mut_sym = &alt.lex_symbols[term_j];
+        let term_exclude = LexSymbol::to_term(mut_sym)
+            .ok_or_else(|| CfgMutateError::new(
+                format!("Unable to convert LexSymbol {} to TermSymbol", mut_sym)
+            ))?;
 
-/// Mutates the give alternative using the base set of terminals `terms`
-fn mutate_alt(alt: &mut RuleAlt, terms: &Vec<&TermSymbol>) -> Result<(), CfgMutateError> {
-    let mut indices: Vec<usize> = vec![];
-    for (i, sym) in alt.lex_symbols.iter().enumerate() {
-        if let LexSymbol::Term(_) = sym {
-            indices.push(i);
-        }
+        let new_terms: Vec<&TermSymbol> = self.terms
+            .iter()
+            .filter(|t| (**t).ne(term_exclude))
+            .map(|t| *t)
+            .collect();
+        let new_term = new_terms.choose(&mut thread_rng()).unwrap();
+        alt.lex_symbols[term_j] = LexSymbol::Term((*new_term).clone());
+
+        Ok(cfg)
     }
-    let j = indices.choose(&mut thread_rng()).unwrap();
-    let mut_sym = &alt.lex_symbols[*j];
-    let term_exclude = LexSymbol::to_term(mut_sym)
-        .ok_or_else(|| CfgMutateError::new(
-            format!("Unable to convert LexSymbol {} to TermSymbol", mut_sym)
-        ))?;
-
-    let new_terms: Vec<&TermSymbol> = terms
-        .iter()
-        .filter(|t| (**t).ne(term_exclude))
-        .map(|t| *t)
-        .collect();
-
-    let new_term = new_terms.choose(&mut thread_rng()).unwrap();
-    println!("t_exclude: {}, new_term: {}", term_exclude, new_term);
-    alt.lex_symbols[*j] = LexSymbol::Term((*new_term).clone());
-
-    Ok(())
 }
 
-fn run(base_cfg: &Cfg, cnt: usize) -> Result<Vec<Cfg>, CfgMutateError> {
-    let terminal_alts = terminal_alts_indices(base_cfg);
-    let non_terms: Vec<&String> = terminal_alts.keys().collect();
-    let terms = base_cfg.terminals();
+fn run(base_cfg: &Cfg) -> Result<Vec<Cfg>, CfgMutateError> {
+    let mut cfg_mut = CfgMutation::new(base_cfg);
+    cfg_mut.instantiate();
 
+    let cnt = cfg_mut.mut_cnt() * (cfg_mut.terms.len() - 1);
     let mut mutated_cfgs: Vec<Cfg> = vec![];
     let mut i: usize = 0;
     loop {
-        let nt = non_terms.choose(&mut thread_rng()).unwrap();
-        let nt_indices = terminal_alts.get(*nt).unwrap();
-        let nt_alt_i = nt_indices.choose(&mut thread_rng()).unwrap();
-        let mut cfg = base_cfg.clone();
-        let mut nt_alt = cfg.get_alt_mut(*nt, *nt_alt_i)
-        .ok_or_else(||
-            CfgMutateError::new(
-                format!("Failed to get alternative for non-terminal {} (index: {})",
-                        nt,
-                        nt_alt_i
-                )
-            ))?;
-        mutate_alt(&mut nt_alt, &terms)?;
-        println!("alt: {}: {}", nt, nt_alt);
-        println!("cfg: {}", cfg);
-        if ! mutated_cfgs.contains(&cfg) {
+        let cfg = cfg_mut.mutate()?;
+        if !mutated_cfgs.contains(&cfg) {
             mutated_cfgs.push(cfg);
+            eprint!(".");
+        } else {
+            eprint!("X");
         }
+        std::io::stdout().flush().unwrap();
 
         i += 1;
-        if i >= cnt {
+        if (i >= MAX_ITER_CNT)  {
             break;
         }
     }
@@ -111,13 +166,21 @@ fn run(base_cfg: &Cfg, cnt: usize) -> Result<Vec<Cfg>, CfgMutateError> {
 mod tests {
     use crate::cfg::mutate::run;
     use crate::cfg::parse;
+    use std::path::Path;
 
     #[test]
     fn test_single_mutation() {
         let cfg = parse::parse("./grammars/lr1.y")
             .expect("Unable to parse as a cfg");
         let cnt: usize = 3;
-        let _ = run(&cfg, cnt)
+        let cfgs = run(&cfg)
             .expect("Unable to generate a mutated cfg");
+        println!("\n=> generated {} cfgs, writing ...", cfgs.len());
+        let basep = Path::new("/var/tmp/cfgtest");
+        for (i, cfg) in cfgs.iter().enumerate() {
+            println!("cfg:\n{}", cfg);
+            std::fs::write(basep.join(i.to_string()), cfg.to_string())
+                .expect(&format!("Failed to write cfg {}", cfg));
+        }
     }
 }
